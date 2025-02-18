a2ce6419d9f1e6d73cddb0e65a5dec0f08aa38c8
Franco Azame <azamefranco@gmail.com>
2025-02-18 18:35:07 -0300

    add function actualizarLista

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const namesList = [];
 function agregarAmigo(){
     const name = document.querySelector(".input-name").value;
     const sizeName = name.length;
-    if(sizeName !== 0 && sizeName >=3 && sizeName <= 20){
-        namesList.push(name);
+    if(sizeName === 0 && sizeName < 3 && sizeName > 20){
+        alert("De ingresar algun nombre, que tenga entre 3 a 20")
         return;
     }
-    alert("De ingresar algun nombre, que tenga entre 3 a 20")
+    actualizarLista(name);
 }
 
+function actualizarLista(newName){
+    const nameListElement = document.querySelector(".name-list");
+    const nameLiElement = document.createElement("li");
+    namesList.push(newName);
+    nameLiElement.textContent = newName;
+    nameListElement.appendChild(nameLiElement);
+}
