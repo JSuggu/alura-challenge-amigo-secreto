e09eb0580373e73c9f63213967ba88ff7947beda
Franco Azame <azamefranco@gmail.com>
2025-02-19 13:43:03 -0300

    fix: fields were not being cleared

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const namesList = [];
+let namesList = [];
 
 function agregarAmigo(){
+    const resultListElem = document.querySelector(".result-list");
+    if(resultListElem.textContent !== "") resultListElem.innerHTML = "";
+
     const name = document.querySelector(".input-name").value;
     const sizeName = name.length;
     if(sizeName === 0 && sizeName < 3 && sizeName > 20){
         alert("De ingresar algun nombre, que tenga entre 3 a 20")
         return;
     }
+    document.querySelector(".input-name").value = "";
     actualizarLista(name);
+    
 }
 
 function actualizarLista(newName){
@@ -29,4 +34,6 @@ function sortearAmigo(){
     const resultListElement = document.querySelector(".result-list");
     document.querySelector(".name-list").innerHTML = "";
     resultListElement.innerHTML = `Tu amigo secreto es: ${winner}`;
+    namesList = [];
 }
+
